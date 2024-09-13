150f463d85ed36accdc4eb1e54dde7209833e2bc
Marco Alonso <marcoalonsorota@hotmail.com>
2024-09-13 12:15:09 -0600

    Initial Commit

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Marco Alonso on 13/09/24.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
